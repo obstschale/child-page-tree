fd9dda50ab6bd5cbf498778c204cf86817760cc1
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2015-08-13 11:40:01 +0200

    add first small test: test_class_exists

--- tests/Child_Page_Tree_Test.php
+++ tests/Child_Page_Tree_Test.php
@@ -9,5 +9,7 @@ namespace Child_Page_Tree;
  */
 class Child_Page_Tree_Test extends \PHPUnit_Framework_TestCase {
 
-
+	public function test_class_exist() {
+		$this->assertTrue( class_exists( 'Child_Page_Tree' ) );
+	}
 }
