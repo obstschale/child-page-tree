0f4ef275141192daffaa07583e630661d2f2d18a
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2015-08-13 11:33:35 +0200

    Add phpunit

---
+++ tests/Child_Page_Tree_Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Child_Page_Tree;
+
+/**
+ * Class Child_Page_Tree_Test
+ *
+ * @author Hans-Helge Buerger
+ */
+class Child_Page_Tree_Test extends \PHPUnit_Framework_TestCase {
+
+
+}
